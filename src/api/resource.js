5e50a2d38b63ee733ec987f2a537d45d15400824
范玉龙 <13067882143@163.com>
2022-08-16 11:49:48 +0800

    权限角色管理

--- src/api/resource.js
+++ src/api/resource.js
@@ -3,6 +3,7 @@ let path = {
 	goodsList:'index/list',						//插画列表
 	getCate:'picture/getcate',					//获取所有插画分类列表
 	painterIndex:'picture/pictureIndex',		//画库列表
+	menuroleList:'menurole/list',				//角色列表
 }					
 export default{
 	//插画列表
@@ -16,6 +17,10 @@ export default{
 	//画库列表
 	painterIndex(params){
 		return http.get(path.painterIndex, params)
+	},
+	//角色列表
+	menuroleList(params){
+		return http.get(path.menuroleList, params)
 	},
 }
 
---
+++ src/components/table_title.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="table_title">
+		<div class="title_text">{{title_text}}</div>
+		<slot></slot>
+	</div>
+</template>
+<style lang="less" scoped>
+.table_title{
+	border: 1px solid #F0F0F0;
+	background: #FAFAFA;
+	width: 100%;
+	height: 52rem;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding-left: 35rem;
+	padding-right: 20rem;
+	.title_text{
+		color: #333333;
+		font-size: 16rem;
+		font-weight: 500;
+	}
+}
+</style>
+<script>
+	export default{
+		props:{
+			title_text:{
+				type:String,
+				default:''
+			},
+		}
+	}
+</script>
